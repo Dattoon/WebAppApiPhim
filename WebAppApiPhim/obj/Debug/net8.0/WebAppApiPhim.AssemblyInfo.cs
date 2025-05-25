7f8aca6535512048205b1e37e88664d34d4ab0a1
Dattoon <ttmdvhd@gmail.com>
2025-05-25 22:06:51 +0700

    Docker

--- WebAppApiPhim/obj/Debug/net8.0/WebAppApiPhim.AssemblyInfo.cs
+++ WebAppApiPhim/obj/Debug/net8.0/WebAppApiPhim.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAppApiPhim")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3339f289d8f5efef77cad33b58b8688eb8c87504")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97c9f2818f591665c787f2dc1c7abad05d9b2a8e")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAppApiPhim")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAppApiPhim")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
